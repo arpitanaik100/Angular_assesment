bddbac210c87535e82dc97de648960eea09295da
Arpita Naik <arpita.naik100@gmail.com>
2020-12-23 22:18:25 +0530

    added login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angularjs';
+  //form: any[] = [];
+  form: FormGroup;
+
+  
+
+  constructor(private fb: FormBuilder){
+  }
+
+  ngOnInit(){
+    this.form=this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+public submit(){
+  //console.log(this.form.value);
+  console.log('form value',this.form.value);
+  alert("Login Success!");
+
+}
+
+public enableSubmitBtn(): boolean{
+  return this.form.valid;
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialDesignModule } from './material-design/material-design.module';
+//import { MaterialModule } from './material.module';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +22,9 @@ import { AppUiModule } from './app-ui.module';
     MaterialDesignModule,
     AppRoutingModule,
     AppUiModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
